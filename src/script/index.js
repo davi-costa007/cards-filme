0aed557f04656664b111ac2ed5cf74789e519381
davi-costa007 <dm354693@gmail.com>
2025-10-22 11:13:31 -0300

    oi

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,20 @@
-const filmes = [
-    {nome:"Motal Combate", genero:"Açao", imagem:"src/img/logo1.jpeg"},
-     {nome:"Motal Combate", genero:"Açao", imagem:"src/img/logo1.jpeg"}
+const Filmes = [
+    {nome:"Shrek", genero:"Ação", imagem:"src/img/baner1.jpeg"},
+    {nome:"Divertidamente", genero:"desenho", imagem:"src/img/baner2.jpeg"}
 ]
+
+
+
+const filtTurma = filmes.filter(item => item.genero === "comedia");
+
+const listFilmes = filtGenero.map(item => {
+    return `
+        <div class="cards">
+            <img src="${imagem}" alt="">
+        <h1>${}</h1>
+        <h1 class="avalia">5.0 ⭐</h1>
+        </div>
+    `
+}).join("")
+
+document.querySelector("body").innerHTML=listFilmes;
